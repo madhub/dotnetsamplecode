4d1e05843f968b2a5dc30b0db11afae65bdd5cab
madhusudana <b.madhu@outlook.com>
2019-12-11 22:21:42 +0530

    initial commit of promethous .net core metric integration

---
+++ MetricsDemo/WebApi/Controllers/MetricsController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers
+{
+    public class MetricsController : Controller
+    {
+
+
+        // POST: Metrics/Create
+        [HttpPost("metrics/job/{jobname}")]
+        public async Task<ActionResult> Post(string jobname)
+        {
+            Console.WriteLine(jobname);
+            var headers = Request.Headers.Select(x =>
+           {
+               return $"Key={x.Key}, Value = {x.Value.ToString()}";
+           });
+            Console.WriteLine($" madhu --- Header {string.Join(",",headers)}");
+            if (Request.Body != null)
+            {
+                using (var reader = new StreamReader(Request.Body,Encoding.ASCII))
+                {
+                    var value = await reader.ReadToEndAsync();
+                    Console.WriteLine($"Madhu--{value}");
+                }
+            }
+            
+           return new OkResult();
+        }
+    }
+}
